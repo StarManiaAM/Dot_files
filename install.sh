a6ac85a5199f341a75f538c7b6b399a150af52d1
StarManiaAM <97359584+StarManiaAM@users.noreply.github.com>
2024-01-05 00:04:22 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,26 +1,26 @@
-#!/bin/sh
+!/bin/sh
 
 # Arch linux installation script
 
 # Mandatory packages
-pacman -S git man unzip python3 nodejs --noconfirm
+sudo pacman -S git man unzip python3 nodejs --noconfirm
 
 # Enable AUR
-git clone https://aur.archlinux.org/yay-git.git && cd yay-git && makepkg -si && cd -
+git clone https://aur.archlinux.org/yay-git.git /tmp/yay-git && cd /tmp/yay-git && makepkg -si && cd -
 
 # Configuration files
-git clone git@github.com:StarManiaAM/Dot_files.git
+git clone git@github.com:StarManiaAM/Dot_files.git /tmp/Dot_files
 mkdir -p .config/alacritty
-cp Dot_files/alacritty.toml ~/.config/alacritty
+cp /tmp/Dot_files/alacritty.toml ~/.config/alacritty
 
 # ZSH installation
 sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 chsh -s /bin/zsh
 
 # Alacritty font configuration
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/JetBrainsMono.zip
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/JetBrainsMono.zip -O /tmp/JetBrainsMono.zip
 mkdir -p /usr/local/share/fonts/ttf/JetBrainsMonoNerd
-unzip JetBrainsMono.zip /usr/local/share/fonts/ttf/JetBrainsMonoNerd
+unzip /tmp/JetBrainsMono.zip -d /usr/local/share/fonts/ttf/JetBrainsMonoNerd
 
 ## Plugins zsh
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
@@ -30,11 +30,11 @@ source ./zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 # Vim installation
-pacman -S neovim --noconfirm
+sudo pacman -S neovim --noconfirm
 git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
-cp Dot_files/nvim/lua/plugins/init.lua .config/nvim/lua/plugins/init.lua
-cp Dot_files/nvim/lua/core/init.lua .config/nvim/lua/core/init.lua
-cp Dot_files/nvim/lua/custom/plugins/lua .config/nvim/lua/.custom/plugins.lua
-cp Dot_files/nvim/lua/core/default_config.lua .config/nvim/lua/core/default_config.lua
+
+cp /tmp/Dot_files/nvim/lua/plugins/init.lua .config/nvim/lua/plugins/init.lua
+cp /tmp/Dot_files/nvim/lua/core/init.lua .config/nvim/lua/core/init.lua
+cp /tmp/Dot_files/nvim/lua/core/default_config.lua .config/nvim/lua/core/default_config.lua
 
 nvim
