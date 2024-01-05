59fc4313067ba2210c6e55c41b4b369b6bb83ea2
Amory Marchal <amory.marchal@epita.fr>
2024-01-05 22:26:15 +0100

    update: install.sh

--- install.sh
+++ install.sh
@@ -9,11 +9,6 @@ sudo pacman -S git man alacritty neovim zsh unzip python3 nodejs --noconfirm
 # Enable AUR
 git clone https://aur.archlinux.org/yay-git.git /tmp/yay-git && cd /tmp/yay-git && makepkg -si --noconfirm && cd -
 
-# ZSH installation
-sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
-cp zshrc ~/.zshrc
-chsh -s /bin/zsh
-
 # Configuration files
 mkdir -p ~/.config/alacritty
 cp alacritty.toml ~/.config/alacritty/alacritty.toml
@@ -21,15 +16,7 @@ cp alacritty.toml ~/.config/alacritty/alacritty.toml
 # Alacritty font configuration
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/JetBrainsMono.zip -O /tmp/JetBrainsMono.zip
 sudo mkdir -p /usr/local/share/fonts/ttf/JetBrainsMonoNerd
-sudo unzip /tmp/JetBrainsMono.zip -d /usr/local/share/fonts/ttf/JetBrainsMonoNerd
-
-## Plugins zsh
-cd /tmp
-git clone https://aur.archlinux.org/zsh-syntax-highlighting-git.git
-cd zsh-syntax-highlighting-git && makepkg -si --noconfirm
-cd -
-echo "source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" ~/.zshrc
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+sudo unzip /tmp/JetBrainsMono.zip -d /usr/local/share/fonts/ttf/JetBrainsMonoNer
 
 # Vim installation
 git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
@@ -37,3 +24,8 @@ git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
 cp nvim/lua/plugins/init.lua ~/.config/nvim/lua/plugins/init.lua
 cp nvim/lua/core/init.lua ~/.config/nvim/lua/core/init.lua
 cp nvim/lua/core/default_config.lua ~/.config/nvim/lua/core/default_config.lua
+
+# ZSH installation
+sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+cp zshrc ~/.zshrc
+chsh -s /bin/zsh
