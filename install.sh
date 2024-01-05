7125eec5d450ea96522c5722870bae5f92148364
StarManiaAM <97359584+StarManiaAM@users.noreply.github.com>
2024-01-05 20:56:38 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,24 +3,19 @@
 # Arch linux installation script
 
 # Mandatory packages
-sudo pacman -S git man unzip python3 nodejs alacritty --noconfirm
+sudo pacman -S git man alacritty neovim zsh unzip python3 nodejs --noconfirm
 
 # Enable AUR
-git clone https://aur.archlinux.org/yay-git.git /tmp/yay-git && cd /tmp/yay-git && makepkg -si && cd -
+git clone https://aur.archlinux.org/yay-git.git /tmp/yay-git && cd /tmp/yay-git && makepkg -si --noconfirm && cd -
 
 # Configuration files
-git clone git@github.com:StarManiaAM/Dot_files.git /tmp/Dot_files
 mkdir -p .config/alacritty
-cp /tmp/Dot_files/alacritty.toml ~/.config/alacritty
-
-# ZSH installation
-sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
-chsh -s /bin/zsh
+cp Dot_files/alacritty.toml ~/.config/alacritty/alacritty.toml
 
 # Alacritty font configuration
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/JetBrainsMono.zip -O /tmp/JetBrainsMono.zip
-mkdir -p /usr/local/share/fonts/ttf/JetBrainsMonoNerd
-unzip /tmp/JetBrainsMono.zip -d /usr/local/share/fonts/ttf/JetBrainsMonoNerd
+sudo mkdir -p /usr/local/share/fonts/ttf/JetBrainsMonoNerd
+sudo unzip /tmp/JetBrainsMono.zip -d /usr/local/share/fonts/ttf/JetBrainsMonoNerd
 
 ## Plugins zsh
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
@@ -30,11 +25,12 @@ source ./zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 # Vim installation
-sudo pacman -S neovim --noconfirm
 git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
 
-cp /tmp/Dot_files/nvim/lua/plugins/init.lua .config/nvim/lua/plugins/init.lua
-cp /tmp/Dot_files/nvim/lua/core/init.lua .config/nvim/lua/core/init.lua
-cp /tmp/Dot_files/nvim/lua/core/default_config.lua .config/nvim/lua/core/default_config.lua
+cp Dot_files/nvim/lua/plugins/init.lua .config/nvim/lua/plugins/init.lua
+cp Dot_files/nvim/lua/core/init.lua .config/nvim/lua/core/init.lua
+cp Dot_files/nvim/lua/core/default_config.lua .config/nvim/lua/core/default_config.lua
 
-nvim
+# ZSH installation
+sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+chsh -s /bin/zsh
