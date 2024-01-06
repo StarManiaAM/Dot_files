b017c3ceabb61f6fe39184efc06198dc6fd88eab
Amory Marchal <amory.marchal@epita.fr>
2024-01-06 15:55:13 +0100

    update:zsh plugins install

--- zsh_plugins_install.sh
+++ zsh_plugins_install.sh
@@ -1,8 +1,7 @@
+cp zshrc ~/.zshrc
 cd /tmp
 git clone https://aur.archlinux.org/zsh-syntax-highlighting-git.git
 cd zsh-syntax-highlighting-git && makepkg -si --noconfirm
 cd -
-echo "source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" ~/.zshrc
+echo "source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
-cp zshrc ~/.zshrc
