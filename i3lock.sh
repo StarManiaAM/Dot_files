ff0218dd2f4652f0d069ecc0d0a8b294eef1b7cd
Amory Marchal <amory.marchal@epita.fr>
2024-05-16 19:03:52 +0200

    update scripts

--- i3lock.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-sudo pacman -R i3lock
-
-yay i3lock-color
-
-mkdir ~/.config/scripts
-
-cp lock ~/.config/scripts/lock
--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 sudo pacman -Syyu --noconfirm
 
 # Mandatory packages
-sudo pacman -S git man alacritty vim neovim zsh unzip python3 nodejs bat bitwarden clang discord docker docker-compose eog feh neofetch net-tools os-prober maim xclip picom  --noconfirm
+sudo pacman -S git man alacritty neovim zsh unzip python3 nodejs bat bitwarden clang discord docker docker-compose eog feh neofetch net-tools os-prober maim xclip picom xorg-xinput  --noconfirm
 
 # Enable AUR
 git clone https://aur.archlinux.org/yay-git.git /tmp/yay-git && cd /tmp/yay-git && makepkg -si --noconfirm && cd -
@@ -12,3 +12,9 @@ git clone https://aur.archlinux.org/yay-git.git /tmp/yay-git && cd /tmp/yay-git 
 # i3 config
 mkdir -p ~/.config/i3
 cp config ~/.config/i3/config
+
+sudo pacman -R i3lock
+
+yay i3lock-color
+
+cp scripts ~/.scripts
