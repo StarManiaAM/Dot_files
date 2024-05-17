24840819e8ee83a4007e9fcb4bec4494259698b6
Amory Marchal <marchal.amory@gmail.com>
2024-05-17 22:33:44 +0200

    scripts updated

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 sudo pacman -Syyu --noconfirm
 
 # Mandatory packages
-sudo pacman -S git man alacritty neovim zsh unzip python3 nodejs bat bitwarden clang discord docker docker-compose eog feh neofetch net-tools os-prober maim xclip picom xorg-xinput  --noconfirm
+sudo pacman -S git man alacritty neovim zsh unzip python3 nodejs bat bitwarden clang discord docker docker-compose eog feh neofetch net-tools os-prober maim xclip picom xorg-xinput dunst  --noconfirm
 
 # Enable AUR
 git clone https://aur.archlinux.org/yay-git.git /tmp/yay-git && cd /tmp/yay-git && makepkg -si --noconfirm && cd -
@@ -17,4 +17,5 @@ sudo pacman -R i3lock
 
 yay i3lock-color
 
-cp scripts ~/.scripts
+cp -r scripts ~/.scripts
+cp wallpaper.jpg ~/Pictures/wallpaper.jpg
