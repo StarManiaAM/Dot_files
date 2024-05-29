79fc7f1768ca6c1e2f807a2922970dc1b97dcc9f
Amory Marchal <amory.marchal@epita.fr>
2024-05-29 12:16:39 +0200

    zsh plugins

--- zsh_config.sh
+++ zsh_config.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-
 cp zshrc ~/.zshrc
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+cd /tmp
+git clone https://aur.archlinux.org/zsh-syntax-highlighting-git.git
+cd zsh-syntax-highlighting-git && makepkg -si --noconfirm
+cd -
+echo "source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 source ~/.zshrc
